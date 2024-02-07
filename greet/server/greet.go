2b514a1a76f5fbf9c7a53398349a7341ba4136eb
Azfar Inan <azfar.inan0615@gmail.com>
2024-02-08 01:12:08 +0600

    Unary API Server Implementation

---
+++ greet/server/greet.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/AzfarInan/grpc-go-course/greet/proto"
+)
+
+func (s *Server) Greet(ctx context.Context, req *pb.GreetRequest) (*pb.GreetResponse, error) {
+
+	log.Printf("Greet function was invoked with %v", req)
+
+	return &pb.GreetResponse{
+		Result: "Hello " + req.FirstName,
+	}, nil
+
+}
