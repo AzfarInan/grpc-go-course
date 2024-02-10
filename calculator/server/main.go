bbec1849ccab5682610d4e7bdd150af945158ccf
Azfar Inan <azfar.inan0615@gmail.com>
2024-02-10 21:29:42 +0600

    Added Evans CLI Support

--- calculator/server/main.go
+++ calculator/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	pb "github.com/AzfarInan/grpc-go-course/calculator/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -25,6 +26,8 @@ func main() {
 
 	s := grpc.NewServer()
 	pb.RegisterCalculatorServer(s, &Server{})
+	// For Evans CLI
+	reflection.Register(s)
 
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
